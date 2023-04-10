b71651cf5a57b1a2001dd98dd0f0bcae20d76ffc
flooper68 <premysl.ciompa@gmail.com>
2023-04-10 20:17:01 +0200

    refactor: adding document

---
+++ packages/shared-immutable-domain/src/document.ts
@@ -0,0 +1,31 @@
+import { Subject } from 'rxjs'
+import { DocumentReducer } from './document/reducer.js'
+import { DocumentEvent, DocumentState } from './document/types.js'
+
+export class Document {
+  private _state: DocumentState
+  private _subject$ = new Subject<DocumentEvent>()
+
+  public eventStream$ = this._subject$.asObservable()
+
+  constructor(initialState: DocumentState) {
+    this._state = initialState
+  }
+
+  private _dispatch(event: DocumentEvent) {
+    this._state = DocumentReducer.reduce(event, this._state)
+    this._subject$.next(event)
+  }
+
+  getState() {
+    return this._state
+  }
+
+  getNode(uuid: string) {
+    return this._state.nodes[uuid]
+  }
+
+  commitEvent(event: DocumentEvent) {
+    this._dispatch(event)
+  }
+}
